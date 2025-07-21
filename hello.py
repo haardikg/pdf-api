2dd4b4e7818a6925a61358834519c06bd30c94b6
Haardik Gupta <36541643+haardikg@users.noreply.github.com>
2025-07-21 16:59:32 +0000

    Initial Working

--- hello.py
+++ hello.py
@@ -1,12 +1,20 @@
-# Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
+#Make sure port is set to public in codespaces
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print("it worked")
     return 'Hello World!'
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    print("Headers:", request.headers, flush=True)
+    print("Body:", request.get_data(as_text=True), flush=True)
+    return "OK", 200
+
 if __name__ == '__main__':
-    app.run()
+     app.run()
+
 
